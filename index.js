dbb748c0a98b66b218f8cb2a660d51b0d24c0197
unknown <samjhanaulak@gmail.com>
2022-03-15 13:52:35 -0400

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your Code Here
+let userName = window.prompt ('please enter your name:')
+let userScore =0
+for(let i = 0; i < questions.length; i++){
+//console.log (questions[i])
+let question = questions [i]
+//window.prompt (?)
+let userAnswer = window.prompt (question.text)
+
+//console.log('Correct')
+
+if(userAnswer === question.correctAnswer){
+    userscore=userScore + 10
+}
+}
+window.alert ('your score is: '+userScore)
